f13de8c1da4967518a633dd0255b70cec0c99afa
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2023-10-25 13:40:58 +0100

    fixing a stuipd bug.

--- app/smtplanner.jl
+++ app/smtplanner.jl
@@ -17,3 +17,8 @@ _solutionformula = solve(roverdomain, roverproblem, 100);
 plan1, z3actins = extractplan(_solutionformula);
 @assert length(plan1) > 0
 
+write("$(pwd())/../../test-write", plan1)
+
+
+actions = ["($(Symbol(a.name)) $(join(a.args, " ")))" for a in plan1]
+push!(actions, "; plan cost $(length(actions)) ")
